ee22463440bb23bac98b60fbd540b2b030e6a8eb
Tim Macfarlane <timmacfarlane@gmail.com>
2017-06-25 21:25:29 +0100

    removed timeToSeconds

--- server/timeToSeconds.js
+++ server/timeToSeconds.js
@@ -1,7 +1,2 @@
 module.exports = function timeToSeconds (time) {
-  const match = /^((\d+):)?(\d+):(\d+)$/.exec(time)
-  if (match) {
-    const [, , hours = 0, minutes, seconds] = match
-    return Number(hours) * 3600 + Number(minutes) * 60 + Number(seconds)
-  }
 }
